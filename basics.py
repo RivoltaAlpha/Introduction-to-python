6159f0c06d9aa4896868c209244bb8ac3da5f095
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-05-04 21:35:22 +0300

    Add object-oriented programming examples and remove old crash course script

--- basics.py
+++ basics.py
@@ -1,11 +1,22 @@
-# Variable Declaration
-name = "Alice"      # String
-age = 25            # Integer
-height = 5.9        # Float
-is_student = True   # Boolean
+# Description: This file contains the basic python code snippets.
+# function in python
+def add(a, b):
+    return a + b
 
-# Printing the variables
-print(name)         # Output: Alice
-print(age)          # Output: 25
-print(height)       # Output: 5.9
-print(is_student)   # Output: True
+# calling function
+result_add = add(2, 3)
+print("Result of add(2, 3):", result_add)
+
+def add_and_compare(a, b):
+    sum_ab = a + b
+    if a > b:
+        largest = a
+    elif b > a:
+        largest = b
+    else:
+        largest = None  # Both numbers are equal
+    return sum_ab, largest
+
+# calling function
+result_add_and_compare = add_and_compare(2, 3)
+print("Result of add_and_compare(2, 3):", result_add_and_compare)
